c951dfedcf857f60f74770b762f0eb6a3e060336
samy360 <samu.munoz@duocuc.cl>
2026-06-12 10:15:07 -0400

    Update samy.py
    parte2

--- samy.py
+++ samy.py
@@ -8,3 +8,18 @@ while True:
             break
     except ValueError:
         print("la cantidad no puede ser superior a 30 y debe ser un numero positivo")
+lista_alumnos=[]
+for i in range(cant_alumnos):
+        print("datos del alumno nro",i+1)
+        alumno={}
+        alumno["nombre"]=input("ingrese nombre del alumno")
+        alumno["direccion"]=input("ingrese direccion del alumno")
+        alumno["telefono"]=input("ingrese telefono del alumno")
+        lista_alumnos.append(alumno)
+for alumno in lista_alumnos:
+    print("--------------------")
+    print(alumno["nombre"])
+    print("--------------------")
+    print(alumno["direccion"])
+    print("--------------------")
+    print(alumno["telefono"])
